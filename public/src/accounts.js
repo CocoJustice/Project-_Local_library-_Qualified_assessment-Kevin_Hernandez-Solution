2990e74e5064628bef614249084d8aaf8897b6be
Kevin Hernandez <kellzgold12@yahoo.com>
2023-10-13 12:42:16 -0500

    first commit

---
+++ public/src/accounts.js
@@ -0,0 +1,46 @@
+function findAccountById(accounts, id) {
+  return accounts.find((account) => account.id === id);
+}
+
+function sortAccountsByLastName(accounts) {
+ return accounts.sort((accountA, accountB) => {
+   const lastNameA = accountA.name.last;
+   const lastNameB = accountB.name.last;
+   return lastNameA.toLowerCase() < lastNameB.toLowerCase() ? -1 : 1;
+ });
+}
+
+function getTotalNumberOfBorrows(account, books) {
+ const { id } = account;
+ let total = 0;
+
+ for (let book in books) {
+   const { borrows } = books[book];
+   borrows.forEach((element) => {
+     if (element.id === id) {
+       total++;
+     }
+   });
+}
+ return total;
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+const possessed = [];
+ books.map((book) => {
+   book.borrows.map((borrow) => {
+     authors.map((author) => {
+      if (author.id === book.authorId) book["author"] = author;  });
+     if (borrow.returned === false && borrow.id === account.id) {
+       possessed.push(book); }
+   });
+ });
+ return possessed;
+}
+
+module.exports = {
+ findAccountById,
+ sortAccountsByLastName,
+ getTotalNumberOfBorrows,
+ getBooksPossessedByAccount,
+};
---
+++ public/src/home.js
@@ -0,0 +1,29 @@
+function getTotalBooksCount(books) {
+  return books.length; 
+}
+
+function getTotalAccountsCount(accounts) {
+  return accounts.length; 
+}
+
+function getBooksBorrowedCount(books) {
+  let borrowedBooks=0;
+  books.forEach(book=> {
+   if(!book.borrows[0].returned)borrowedBooks++;
+  })
+  return borrowedBooks;
+}
+function getMostCommonGenres(books) {}
+
+function getMostPopularBooks(books) {}
+
+function getMostPopularAuthors(books, authors) {}
+
+module.exports = {
+  getTotalBooksCount,
+  getTotalAccountsCount,
+  getBooksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
